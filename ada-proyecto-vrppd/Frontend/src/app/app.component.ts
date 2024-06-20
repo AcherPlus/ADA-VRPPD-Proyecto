50caed323e12b0ab7948bd2b230305cf819ad2f3
Brunitozz <122190257+Brunitozz@users.noreply.github.com>
2024-06-20 11:44:30 -0500

    "Added Angular Forms modules and implemented form validation in InicioSesionComponent"

--- ada-proyecto-vrppd/Frontend/src/app/app.component.ts
+++ ada-proyecto-vrppd/Frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PaginaInicioComponent } from "./pagina-inicio/pagina-inicio.component";
 import { InicioSesionGeneralComponent } from "./inicio-sesion-general/inicio-sesion-general.component";
 import {InicioSesionComponent} from "./proveedor/inicio-sesion/inicio-sesion.component";
@@ -11,6 +12,8 @@ import {InicioSesionComponent} from "./proveedor/inicio-sesion/inicio-sesion.com
     styleUrl: './app.component.css',
     imports: [
       RouterOutlet, 
+      FormsModule,
+      ReactiveFormsModule,
       PaginaInicioComponent, 
       InicioSesionGeneralComponent,
       InicioSesionComponent
--- ada-proyecto-vrppd/Frontend/src/app/proveedor/inicio-sesion/inicio-sesion.component.ts
+++ ada-proyecto-vrppd/Frontend/src/app/proveedor/inicio-sesion/inicio-sesion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio-sesion',
@@ -8,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './inicio-sesion.component.css'
 })
 export class InicioSesionComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      dni: ['', [Validators.required, Validators.pattern('\\d+')]],
+      password: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const dni = this.loginForm.get('dni')?.value;
+      const password = this.loginForm.get('password')?.value;
+      console.log('DNI:', dni);
+      console.log('Password:', password);
+      // process and send to API
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
