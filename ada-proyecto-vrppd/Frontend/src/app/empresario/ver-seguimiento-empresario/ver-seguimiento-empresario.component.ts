d60eea666faeed6188eef56373d1158a8a4e7238
AcherPlus <enmanuel.obando@unmsm.edu.pe>
2024-06-21 17:01:27 -0500

    seguimiento y envio empresario

--- ada-proyecto-vrppd/Frontend/src/app/empresario/ver-seguimiento-empresario/ver-seguimiento-empresario.component.ts
+++ ada-proyecto-vrppd/Frontend/src/app/empresario/ver-seguimiento-empresario/ver-seguimiento-empresario.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarBipComponent } from "../../navbar-bip/navbar-bip.component";
 
 @Component({
-  selector: 'app-ver-seguimiento-empresario',
-  standalone: true,
-  imports: [],
-  templateUrl: './ver-seguimiento-empresario.component.html',
-  styleUrl: './ver-seguimiento-empresario.component.css'
+    selector: 'app-ver-seguimiento-empresario',
+    standalone: true,
+    templateUrl: './ver-seguimiento-empresario.component.html',
+    styleUrl: './ver-seguimiento-empresario.component.css',
+    imports: [NavbarBipComponent]
 })
 export class VerSeguimientoEmpresarioComponent {
 
