6963a2bc4ec2c178b4756de3941a8dfeb82f9745
Miguel Angel Vera Alva <112670367+itsantivirus@users.noreply.github.com>
2024-07-03 18:15:59 -0500

    conectado el back con front

--- ada-proyecto-vrppd/Backend/Locales/models.py
+++ ada-proyecto-vrppd/Backend/Locales/models.py
@@ -1,18 +1,33 @@
 from django.db import models
 from Empresarios.models import Empresario
 from Productos.models import Producto
+from geopy.geocoders import Nominatim
 
 # Create your models here.
 class Local(models.Model):
     idlocal = models.AutoField(primary_key=True)
-    ubicacion = models.CharField()
-    nombre = models.CharField()
-    idempresaio = models.ForeignKey(Empresario, models.DO_NOTHING, db_column='idempresaio')
+    direccion = models.CharField(blank=True, null=True)
+    nombrelocal = models.CharField()
+    idempresario = models.ForeignKey(Empresario, models.DO_NOTHING, db_column='idempresario')
+    distrito = models.CharField(blank=True, null=True)
+    region = models.CharField(blank=True, null=True)
+    longitud = models.FloatField()
+    latitud = models.FloatField()
+
+    def save(self, *args, **kwargs):
+        if not self.direccion or not self.distrito or not self.region:
+            geolocator = Nominatim(user_agent="myGeocoder")
+            location = geolocator.reverse((self.latitud, self.longitud), exactly_one=True)
+            if location:
+                address = location.raw.get('address', {})
+                self.direccion = f"{address.get('road', '')} {address.get('house_number', '')}"
+                self.distrito = address.get('suburb', '') or address.get('city_district', '') or address.get('neighbourhood', '')
+                self.region = address.get('state', '') or address.get('region', '') or address.get('county', '')
+        super().save(*args, **kwargs)
 
     class Meta:
         managed = False
         db_table = 'local'
-
 
 class Localproducto(models.Model):
     idlocalprod = models.AutoField(primary_key=True)
