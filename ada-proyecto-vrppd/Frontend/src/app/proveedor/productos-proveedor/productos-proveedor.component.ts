bc1d0202276061caad5abddea17e3ccb0279394f
AcherPlus <enmanuel.obando@unmsm.edu.pe>
2024-07-04 23:04:55 -0500

    productos y quitar ruc

--- ada-proyecto-vrppd/Frontend/src/app/proveedor/productos-proveedor/productos-proveedor.component.ts
+++ ada-proyecto-vrppd/Frontend/src/app/proveedor/productos-proveedor/productos-proveedor.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarBipComponent } from '../../navbar-bip/navbar-bip.component';
+import { ProductoService } from '../../services/producto.service';
 @Component({
   selector: 'app-productos-proveedor',
   standalone: true,
@@ -8,6 +9,21 @@ import { NavbarBipComponent } from '../../navbar-bip/navbar-bip.component';
   templateUrl: './productos-proveedor.component.html',
   styleUrl: './productos-proveedor.component.css'
 })
-export class ProductosProveedorComponent {
+export class ProductosProveedorComponent implements OnInit{
 
+  dataProductos: any[] = [];
+
+  constructor(private productoService: ProductoService) { }
+
+  ngOnInit(): void {
+    this.getProductos();
+  }
+
+  getProductos(){
+    this.productoService.getProductos().subscribe(data => {
+      this.dataProductos = data;
+    });
+
+    console.log(this.dataProductos);
+  }
 }
