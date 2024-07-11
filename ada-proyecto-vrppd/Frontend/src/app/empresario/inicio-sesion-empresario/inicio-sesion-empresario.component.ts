810b06f0da703182f51818564f6c1a4ed875b7b1
RichardTorresRivera <rmtr2711@gmail.com>
2024-07-11 00:09:43 -0500

    login empresario

--- ada-proyecto-vrppd/Frontend/src/app/empresario/inicio-sesion-empresario/inicio-sesion-empresario.component.ts
+++ ada-proyecto-vrppd/Frontend/src/app/empresario/inicio-sesion-empresario/inicio-sesion-empresario.component.ts
@@ -1,13 +1,55 @@
+// inicio-sesion-empresario.component.ts
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { EmpresarioService } from '../../services/empresario.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio-sesion-empresario',
-  standalone: true,
-  imports: [RouterModule],
   templateUrl: './inicio-sesion-empresario.component.html',
-  styleUrl: './inicio-sesion-empresario.component.css'
+  styleUrls: ['./inicio-sesion-empresario.component.css'],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
+  standalone: true,
 })
 export class InicioSesionEmpresarioComponent {
+  loginForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private empresarioService: EmpresarioService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      dni: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
 
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const username = this.loginForm.get('dni')?.value;
+      const password = this.loginForm.get('password')?.value;
+      this.empresarioService.login(username, password).subscribe({
+        next: (success) => {
+          if (success) {
+            this.empresarioService.setLoginStatus(true);
+            this.router.navigate(['/inicio-empresario']);
+          } else {
+            alert('usuario o pass incorrecto');
+          }
+        },
+        error: (error) => {
+          alert('Error en el incio');
+          console.error('Error', error);
+        },
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
--- ada-proyecto-vrppd/Frontend/src/app/services/empresario.service.ts
+++ ada-proyecto-vrppd/Frontend/src/app/services/empresario.service.ts
@@ -1,9 +1,40 @@
+// empresario.service.ts
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmpresarioService {
+  private apiUrl = 'http://127.0.0.1:8000/api/empresarios/empresarios/';
+  constructor(private http: HttpClient) {}
+  getEmpresarios(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
 
-  constructor() { }
+  login(username: string, password: string): Observable<boolean> {
+    return new Observable<boolean>((observer) => {
+      this.getEmpresarios().subscribe((empresarios) => {
+        const user = empresarios.find(
+          (emp: any) =>
+            emp.nombre_usuario_emp === username && emp.password_emp === password
+        );
+        observer.next(!!user);
+        observer.complete();
+      });
+    });
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('isLoggedIn');
+  }
+
+  setLoginStatus(status: boolean): void {
+    localStorage.setItem('isLoggedIn', status ? 'true' : 'false');
+  }
+
+  logout(): void {
+    localStorage.removeItem('isLoggedIn');
+  }
 }
