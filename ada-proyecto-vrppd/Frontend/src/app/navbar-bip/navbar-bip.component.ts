73c363769138f53f8d61269fa498541b62114d58
Brunitozz <122190257+Brunitozz@users.noreply.github.com>
2024-07-10 23:13:24 -0500

    refactor: Update logout button in navbar-bip component

--- ada-proyecto-vrppd/Frontend/src/app/navbar-bip/navbar-bip.component.ts
+++ ada-proyecto-vrppd/Frontend/src/app/navbar-bip/navbar-bip.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-navbar-bip',
@@ -9,5 +10,9 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navbar-bip.component.css'
 })
 export class NavbarBipComponent {
+  constructor(private authService: AuthService) { }
 
+  logout(): void {
+    this.authService.logout();
+  }
 }
