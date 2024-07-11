2397357787cca6108b71d4e1e35b4ea38758b9a4
Brunitozz <122190257+Brunitozz@users.noreply.github.com>
2024-07-11 01:51:13 -0500

    chore: Add @types/jquery dependency and update package-lock.json

--- ada-proyecto-vrppd/Frontend/src/app/proveedor/almacenes-proveedor/almacenes-proveedor.component.ts
+++ ada-proyecto-vrppd/Frontend/src/app/proveedor/almacenes-proveedor/almacenes-proveedor.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { NavbarBipComponent } from '../../navbar-bip/navbar-bip.component';
+import { AlmacenService } from '../../services/almacen.service';
+import { HttpClient } from '@angular/common/http';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-almacenes-proveedor',
   standalone: true,
-  imports: [RouterModule, NavbarBipComponent],
+  imports: [RouterModule, NavbarBipComponent, NgFor],
   templateUrl: './almacenes-proveedor.component.html',
-  styleUrl: './almacenes-proveedor.component.css'
+  styleUrl: './almacenes-proveedor.component.css',
 })
 export class AlmacenesProveedorComponent {
+  constructor(private almacenService: AlmacenService, private router: Router) {}
+  ubicaciones: any[] = [];
+  ngOnInit(): void {
+    this.cargarUbicaciones();
+  }
+  cargarUbicaciones(): void {
+    this.almacenService.getUbicaciones().subscribe(
+      (data: any) => {
+        this.ubicaciones = data;
+      },
+      (error: any) => {
+        console.error('Error al cargar las ubicaciones', error);
+      }
+    );
+  }
 
 }
