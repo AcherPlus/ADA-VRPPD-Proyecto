67fda6e3d8ea0ea490eca9f1ad0a230300d0719f
RichardTorresRivera <rmtr2711@gmail.com>
2024-07-11 09:21:06 -0500

    xcddddddd

--- ada-proyecto-vrppd/Frontend/src/app/services/local.service.ts
+++ ada-proyecto-vrppd/Frontend/src/app/services/local.service.ts
@@ -1,22 +1,20 @@
 import { HttpClient } from '@angular/common/http';
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocalService {
-
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   private urlLocal = 'http://127.0.0.1:8000/api/locales/locales/';
 
-  getLocales(): Observable<any>{
+  getLocales(): Observable<any> {
     return this.http.get<any>(this.urlLocal);
   }
 
-  addLocal(local: any): Observable<any>{
+  addLocal(local: any): Observable<any> {
     return this.http.post<any>(this.urlLocal, local);
   }
 }
